f22d66195b15835371dcefbb5a5df099b1cb4e7a
wekers <fernando@wekers.org>
2023-05-24 21:08:44 -0300

    First -Categoria

---
+++ src/main/java/com/devsuperior/desafioessential02/DesafioEssential02Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioessential02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEssential02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEssential02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafioessential02/DesafioEssential02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioessential02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEssential02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

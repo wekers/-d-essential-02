c3415d68fe1a9cb17b4b71db5ae659f398cc6def
Fernando Gilli <fernando@wekers.org>
2023-05-26 16:41:30 -0300

    Classe Participante

---
+++ src/main/java/com/devsuperior/desafioessential02/entities/Participante.java
@@ -0,0 +1,74 @@
+package com.devsuperior.desafioessential02.entities;
+
+import jakarta.persistence.*;
+
+import java.util.*;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+
+    @ManyToMany(mappedBy = "participantes")
+    private Set<Atividade> atividades = new HashSet<>();
+
+    public Participante(){
+
+    }
+
+    public Participante(Integer id, String nome, String email, Set<Atividade> atividades) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.atividades = atividades;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Atividade> getAtividades() {
+        return atividades;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Participante that = (Participante) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}

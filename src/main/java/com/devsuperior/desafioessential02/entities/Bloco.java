c24bdbaaddb88e2d3cdbb4aedb8fba978024c757
Fernando Gilli <fernando@wekers.org>
2023-05-26 16:42:04 -0300

    Classe Bloco

---
+++ src/main/java/com/devsuperior/desafioessential02/entities/Bloco.java
@@ -0,0 +1,77 @@
+package com.devsuperior.desafioessential02.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant inicio;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant fim;
+
+    @ManyToOne
+    @JoinColumn(name = "atividade_id")
+    private Atividade atividade;
+
+    public Bloco(){
+
+    }
+
+    public Bloco(Integer id, Instant inicio, Instant fim, Atividade atividade) {
+        this.id = id;
+        this.inicio = inicio;
+        this.fim = fim;
+        this.atividade = atividade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Instant inicio) {
+        this.inicio = inicio;
+    }
+
+    public Instant getFim() {
+        return fim;
+    }
+
+    public void setFim(Instant fim) {
+        this.fim = fim;
+    }
+
+    public Atividade getAtividade() {
+        return atividade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Bloco bloco = (Bloco) o;
+
+        return Objects.equals(id, bloco.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}

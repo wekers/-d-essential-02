08c7bc7dfa642ed8f8c17f8efcd055833aa54366
Fernando Gilli <fernando@wekers.org>
2023-05-26 16:41:48 -0300

    Classe Categoria

--- src/main/java/com/devsuperior/desafioessential02/entities/Categoria.java
+++ src/main/java/com/devsuperior/desafioessential02/entities/Categoria.java
@@ -2,6 +2,8 @@ package com.devsuperior.desafioessential02.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -12,6 +14,9 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String descricao;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Atividade> atividades = new ArrayList<>();
 
     public Categoria(){
 
@@ -36,6 +41,10 @@ public class Categoria {
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+
+    public List<Atividade> getAtividades() {
+        return atividades;
     }
 
     @Override
